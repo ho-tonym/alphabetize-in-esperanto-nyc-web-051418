4dd99131e9d5cd5058f9512f110502952f8554b9
Tony Ho <ho.tonym@gmail.com>
2018-05-13 17:18:32 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,14 @@
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
 def alphabetize(arr)
-  # code here
+
+    esperanto_array = ESPERANTO_ALPHABET.split("")
+    letters_array = []
+
+    arr.sort_by do |sentence|
+        letters_array = sentence.split("")
+        letters_array.map do |letter|
+          esperanto_array.index(letter)
+        end
+    end
 end
